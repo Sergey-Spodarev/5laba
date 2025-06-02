48e4da292ab5f432dbe959cc03238d7672501a09
Sergey-Spodarev <spodarev8@gmail.com>
2025-06-02 20:33:16 +0300

    wrote a Java doc while waiting for the unit test

--- src/test/java/InjectorTest.java
+++ src/test/java/InjectorTest.java
@@ -3,7 +3,13 @@ import somepackage.SomeBean;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+/**
+ * Тесты для проверки работы механизма внедрения зависимостей.
+ */
 public class InjectorTest {
+    /**
+     * Проверяет, что зависимости корректно внедряются из файла depsA.properties.
+     */
     @Test
     public void testInjectionWithSomeImpl() {
         SomeBean sb = new Injector("depsA.properties").inject(new SomeBean());
@@ -11,6 +17,9 @@ public class InjectorTest {
         assertDoesNotThrow(sb::foo);
     }
 
+    /**
+     * Проверяет, что зависимости корректно внедряются из файла depsB.properties.
+     */
     @Test
     public void testInjectionWithOtherImpl() {
         SomeBean sb = new Injector("depsB.properties").inject(new SomeBean());
